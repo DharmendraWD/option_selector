8828f99b2aa72a2245938a137efc8230444d3f1a
DharmendraWD <zaynhere@icloud.com>
2023-10-29 10:13:52 +0545

    ready to push

--- script.js
+++ script.js
@@ -1,71 +1,67 @@
 /*Program to selecte the Given Provinces then shows all the districts which lie in a selected 
 province.*/ 
 
-
 // Provinces Lists 
-let provincesLists = [
+let prvnc = [
    {
-    prov: "Madhesh",
-   dist:["Sarlahi", "Rautahat", "Bara", "Parsa", "Dhanusha", "Siraha", "Mahottari", "Saptari"]
+    p: "Madhesh",
+   d:["Sarlahi", "Rautahat", "Bara", "Parsa", "Dhanusha", "Siraha", "Mahottari", "Saptari"]
    },
    {
-    prov: "Koshi",
-    dist:["Bhojpur", "Dhankuta", "Ilam", "Jhapa", "Khotang", "Morang", "Okhaldhunga", "Panchthar", "Sankhuwasabha", "Solukhumbu", "Sunsari", "Taplejung", "Tehrathum", "Udayapur"]
+    p: "Koshi",
+    d:["Bhojpur", "Dhankuta", "Ilam", "Jhapa", "Khotang", "Morang", "Okhaldhunga", "Panchthar", "Sankhuwasabha", "Solukhumbu", "Sunsari", "Taplejung", "Tehrathum", "Udayapur"]
    }, 
    {
-    prov:"Bagmati",
-    dist:["Sindhuli", "Ramechhap", "Dolakha", "Bhaktapur", "Dhading", "Kathmandu", "Kavrepalanchowk", "Lalitpur", "Nuwakot", "Rasuwa", "Sindhupalchok", "Chitwan", "Makwanpur"]
+    p:"Bagmati",
+    d:["Sindhuli", "Ramechhap", "Dolakha", "Bhaktapur", "Dhading", "Kathmandu", "Kavrepalanchowk", "Lalitpur", "Nuwakot", "Rasuwa", "Sindhupalchok", "Chitwan", "Makwanpur"]
    },
    {
-    prov:"Gandaki",
-    dist:["Baglung", "Gorkah", "Kaski", "Lamjung", "Manang", "Mustang", "Myagdi", "Nawalparasi", "Parbat", "Syangja", "Tanahu"]
+    p:"Gandaki",
+    d:["Baglung", "Gorkah", "Kaski", "Lamjung", "Manang", "Mustang", "Myagdi", "Nawalparasi", "Parbat", "Syangja", "Tanahu"]
    },
    {
-    prov:"Lumbini",
-    dist:["Kapilvastu", "Nawalparasi", "Rupandehi", "Arghakhanchi", "Gulmi", "Palpa", "Dang", "Pyuthan", "Rolpa", "Eastern Rukum", "Banke", "Bardiya"]
+    p:"Lumbini",
+    d:["Kapilvastu", "Nawalparasi", "Rupandehi", "Arghakhanchi", "Gulmi", "Palpa", "Dang", "Pyuthan", "Rolpa", "Eastern Rukum", "Banke", "Bardiya"]
    },
    {
-    prov:"Karnali",
-    dist:["Rukum", "Salyan", "Dolpa", "Humla", "Jumla", "Kalikot", "Mugu", "Surkhet", "Jajarkot", "Dailekh",]
+    p:"Karnali",
+    d:["Rukum", "Salyan", "Dolpa", "Humla", "Jumla", "Kalikot", "Mugu", "Surkhet", "Jajarkot", "Dailekh",]
    },
    {
-    prov:"Sudarpaschim",
-    dist:["Kailali", "Achham", "Doti", "Bajhang", "Bajura", "Kanchanpur", "Dadeldhura", "Baitadi", "Darchula"]
+    p:"Sudarpaschim",
+    d:["Kailali", "Achham", "Doti", "Bajhang", "Bajura", "Kanchanpur", "Dadeldhura", "Baitadi", "Darchula"]
    }
 ]
 
 let ht_Provinces = document.querySelector('#provinces');
 let ht_districts = document.querySelector("#districts")
-// TO SHOW PROVINCES IN PROVINCE BOX 
-provincesLists.forEach(province => {
-    let optionElement = document.createElement("option");
-    optionElement.value = province.prov;
-    optionElement.textContent = province.prov;
-    ht_Provinces.appendChild(optionElement);
-});
-// ----------------------------
-
-// If Changes in Provinces selected option
-ht_Provinces.addEventListener("change", function () {
-    let selectedProvince = ht_Provinces.value;
-    
-    // Clear the district select
-    ht_districts.innerHTML = '<option value="" disabled selected>Districts</option>';
-
-    if (selectedProvince) {
-        // Find the province in the provincesList
-        let matchingProvince = provincesLists.find(province => province.prov === selectedProvince);
-
-        if (matchingProvince) {
-            // Populate the district select with districts from the selected province
-            matchingProvince.dist.forEach(district => {
-                let optionElement = document.createElement("option");
-                optionElement.value = district;
-                optionElement.textContent = district;
-                ht_districts.appendChild(optionElement);
-            });
-        }
-    }
-});
+let options = document.querySelectorAll(".options")
 
 
+// Filling Provinces 
+prvnc.forEach(function(val, index){
+let prvncElem = document.createElement('option')
+prvncElem.innerText=prvnc[index].p
+ht_Provinces.appendChild(prvncElem)
+prvncElem.setAttribute('id', index)
+})
+
+
+ht_Provinces.addEventListener('change', function(){
+    // for Clearing the District after changing option in province 
+    ht_districts.innerHTML= '<option value="" disabled selected>Districts</option>';
+
+  // Get selected Option's Value
+  let selectedOpt =ht_Provinces.options[ht_Provinces.selectedIndex] 
+  
+  // Get selected Option's ID 
+  let selectedId = selectedOpt.id;
+  console.log(selectedId)
+  
+// Filling the value in DIstrict after selecting province 
+ prvnc.forEach(function(v, i){
+let distElem = document.createElement('option');
+distElem.innerText=prvnc[selectedId].d[i];
+ht_districts.appendChild(distElem);
+})
+})
